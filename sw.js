fc1fbb55f105a69b0b949f5e3e35a73e8987b0c1
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2024-03-17 14:46:57 +0900

    [update] feature/#12 Service Worker のコンソールエラーを解消

---
+++ sw.js
@@ -0,0 +1,2 @@
+// Service Worker のキャッシュバージョン名
+const cacheName = "0.20240317.1";  // eslint-disable-line @typescript-eslint/no-unused-vars
--- sw.ts
+++
@@ -1,2 +0,0 @@
-// Service Worker のキャッシュバージョン名
-const cacheName = "0.20240316.2";
