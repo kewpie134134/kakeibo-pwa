28737288b0d35ca89af34b13445ed14d2797b824
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2024-03-23 22:58:49 +0900

    [update] #13 Table のツールバーの表示位置を修正

--- src/molecules/EnhancedTableToolbar.tsx
+++ src/molecules/EnhancedTableToolbar.tsx
@@ -5,14 +5,14 @@ const EnhancedTableToolbar = () => {
   return (
     <Toolbar>
       <Typography
-        sx={{ flex: "1 1 100%" }}
+        sx={{ position: "fixed", left: "50%", transform: "translateX(-50%)" }}
         variant="h6"
         id="tableTitle"
         component="div"
       >
         YYYY年MM月
       </Typography>
-      <Tooltip title="検索">
+      <Tooltip title="検索" sx={{ position: "absolute", right: 24 }}>
         <IconButton>
           <SearchIcon />
         </IconButton>
