35e0d585325a3d2f833738c545dc36ebd74cfd19
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2024-05-01 12:54:53 +0900

    [update] #13 日付を側だけ変更でけいるように修正

--- src/molecules/EnhancedTableToolbar.tsx
+++ src/molecules/EnhancedTableToolbar.tsx
@@ -7,9 +7,18 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { ja } from "date-fns/locale/ja";
 import { useState } from "react";
+import { addMonths } from "date-fns";
 
 const EnhancedTableToolbar = () => {
   const [date, setDate] = useState(new Date());
+
+  const addOneMonthHandler = () => {
+    setDate(addMonths(date, 1));
+  };
+
+  const subOneMonthHandler = () => {
+    setDate(addMonths(date, -1));
+  };
 
   return (
     <Toolbar>
@@ -22,7 +31,7 @@ const EnhancedTableToolbar = () => {
         display="flex"
       >
         <Tooltip title="先月">
-          <IconButton onClick={() => {}}>
+          <IconButton onClick={subOneMonthHandler}>
             <PlayArrowOutlinedIcon
               fontSize="large"
               sx={{ transform: "scale(-1,1)" }}
@@ -36,11 +45,12 @@ const EnhancedTableToolbar = () => {
               defaultValue={date}
               // slotProps={{ textField: { variant: "standard" } }}
               onError={() => {}}
+              value={date}
             />
           </DemoContainer>
         </LocalizationProvider>
         <Tooltip title="来月">
-          <IconButton onClick={() => {}}>
+          <IconButton onClick={addOneMonthHandler}>
             <PlayArrowOutlinedIcon fontSize="large" />
           </IconButton>
         </Tooltip>
